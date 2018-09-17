00d1fd41adcdc61dbcbcb03cf66d49a3bfe39761
Reng Tsai <rengah0@gmail.com>
2018-09-17 22:41:58 +0800

    sleep mode update
    -hint text blink

--- SceneSleep.h
+++ SceneSleep.h
@@ -7,11 +7,16 @@
 
 
 class SceneSleep:public SceneBase{
+	
+	ofImage _img_hint;
+	FrameTimer _timer_hint;
 
 public:
 	SceneSleep(ofApp *set_ptr):SceneBase(set_ptr){
 		loadImage();
 		_order_scene=0;
+
+		ofAddListener(SceneBase::sceneInFinish,this,&SceneSleep::onSceneInFinish);
 	}
 	void loadImage(){
 		_img_ui[0].loadImage("ui/back/01.png");
@@ -19,19 +24,46 @@ public:
 		_img_ui[2].loadImage("ui/content/01.png");
 		_img_ui[3].loadImage("ui/button/01.png");
 
+
+		_img_hint.loadImage("ui/content/01-1.png");
+		_timer_hint=FrameTimer(1500);
+
+
 		_button.push_back(ofRectangle(703,2227,641,221));
 	}
 
 	void draw(bool debug_){
 		SceneBase::draw(debug_);
 
+		float alpha_=255*(1.0-_timer_out[3].val())*_timer_in[3].val();
+		ofPushStyle();
+		ofSetColor(255,alpha_*(sin(PI*_timer_hint.val())));
+			_img_hint.draw(0,0);
+		ofPopStyle();
 	}
+	void update(float dt_){
+		SceneBase::update(dt_);
 
+		_timer_hint.update(dt_);
+		if(_timer_hint.val()==1){
+			_timer_hint.restart();
+		}
+	}
 	
 	void buttonEvent(int index){
 		_ptr_app->setScene(ofApp::SceneMode::WISH);
 
 	}
+
+	void onSceneInFinish(int &e){	
+		if(e==_order_scene) _timer_hint.restart();
+	}
+
+	void init(){
+		SceneBase::init();
+		_timer_hint.reset();
+	}
+	
 
 };
 
